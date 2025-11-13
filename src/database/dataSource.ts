e118847f86dcc14266cc49e7da9fcbadeaf64240
DannyGalvan <cgalvan29111999@gmail.com>
2025-11-13 05:25:43 -0600

    chore: update dependencies and version bump to 0.0.2
    - Updated package version from 0.0.1 to 0.0.2 in package.json
    - Upgraded React and React Native to versions 19.1.1 and 0.82.1 respectively
    - Replaced react-native-quick-sqlite with react-native-nitro-sqlite in dataSource.ts
    - Modified tsconfig.json to include and exclude specific directories
    - Added AppDelegate.swift for iOS setup with React Native
    - Updated various dependencies to their latest versions

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,4 +1,4 @@
-import { typeORMDriver } from 'react-native-quick-sqlite';
+import { typeORMDriver } from 'react-native-nitro-sqlite';
 import { DataSource } from 'typeorm';
 import { SessionStorage } from '@database/models/SessionStorage';
 import { NAME_BD } from '@config/constants';
